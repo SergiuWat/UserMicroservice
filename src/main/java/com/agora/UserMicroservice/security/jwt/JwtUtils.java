1b5ca20146a9aaf5ca372e6f02c7d12ee3546be8
Madalina-Tudor <madalina.puia21@gmail.com>
2023-03-28 01:46:42 +0300

    Created deleteUserByEmail Creted updatePassword

--- src/main/java/com/agora/UserMicroservice/security/jwt/JwtUtils.java
+++ src/main/java/com/agora/UserMicroservice/security/jwt/JwtUtils.java
@@ -48,4 +48,12 @@ public class JwtUtils {
         return false;
     }
 
-}
+    public String parseJwt(String token) {
+        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
+    public String getEmailFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+}//Jwt
